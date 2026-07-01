54a2e085646549cd6be620a26fd832a4352a51e7
siyam-hosssain <siyam.cybersoul@gmail.com>
2026-07-01 18:13:58 +0600

    Reference Type

---
+++ 002_Types/003_Reference_Types/ReferenceTypes/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+
+public class Main {
+    public static void main(String[] args){
+//      reference type required allocated memory while primitive don't
+//      it has members
+        Date now = new Date();
+        System.out.println("now: "+now);
+    }
+}
