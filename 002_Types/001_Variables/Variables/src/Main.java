7a9408409aff50b7bbbe631d14355bc1641e616b
siyam-hosssain <siyam.cybersoul@gmail.com>
2026-07-01 17:24:30 +0600

    Variables

---
+++ 002_Types/001_Variables/Variables/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args){
+
+       int age = 30;
+       int temperature = 25;
+       int herAge = age;
+
+       System.out.println("Age: "+age);
+       System.out.println("Temperature: "+temperature);
+       System.out.println("Her age: "+herAge);
+    }
+
+}
