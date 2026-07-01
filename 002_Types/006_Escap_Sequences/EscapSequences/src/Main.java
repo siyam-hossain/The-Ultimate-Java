3569cab67fa3928ccb3c19e3c4bfca490bb815ba
siyam-hosssain <siyam.cybersoul@gmail.com>
2026-07-01 19:45:22 +0600

    Escape Sequences

---
+++ 002_Types/006_Escap_Sequences/EscapSequences/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args){
+        String message = "Hello, \"Siyam\"";
+        System.out.println(message);
+
+//        c:\Windows\..
+        String path = "c:\\Windows\\..";
+        System.out.println(path);
+
+        String m1 = "hello\nworld\t.";
+        System.out.println(m1);
+
+    }
+
+}
