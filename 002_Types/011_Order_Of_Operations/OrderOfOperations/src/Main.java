d736e5b5647b78060f82cc14833d2212da47e055
siyam-hosssain <siyam.cybersoul@gmail.com>
2026-07-01 20:21:53 +0600

    Order of operations

---
+++ 002_Types/011_Order_Of_Operations/OrderOfOperations/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args){
+//        * has highest precedence here for below expression
+        int x = 10 + 3 * 2;
+        System.out.println("x: "+x);
+    }
+}
