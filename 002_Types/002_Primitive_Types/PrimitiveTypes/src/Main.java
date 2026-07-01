a3a551e9d90cdbd227d2c3827fc42e47b410ef9d
siyam-hosssain <siyam.cybersoul@gmail.com>
2026-07-01 17:44:12 +0600

    Primitive types

---
+++ 002_Types/002_Primitive_Types/PrimitiveTypes/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args){
+        byte age = 30;
+        int viewsCount = 123_456_789;
+        long viewsCountLong = 3_123_456_789L;
+        float cgpa = 3.95F;
+        double price = 10.99;
+        char letter = 'A';
+        boolean isEligible = false;
+
+        System.out.println("age: "+age);
+        System.out.println("views count: "+viewsCount);
+        System.out.println("views count for long: "+viewsCountLong);
+        System.out.println("cgpa: "+cgpa);
+        System.out.println("price: "+price);
+        System.out.println("letter: "+letter);
+        System.out.println("is eligible: "+isEligible);
+    }
+}
