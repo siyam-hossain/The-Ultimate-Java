5489fcede18bef5714a60dabb1e10252b6a14ec2
siyam-hosssain <siyam.cybersoul@gmail.com>
2026-07-02 10:28:13 +0600

    Refactoring

---
+++ 004_Clean_Coding/002_Refactoring/Refactoring/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+//        understand the concept
+        /*
+            the process of restructuring existing code to improve its readability, maintainability, and design without changing its external behavior or functionality.
+        */
+    }
+}
