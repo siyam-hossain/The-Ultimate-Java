182fb0fe3ce5c3ae1ba292ce3fb39701aa54369a
siyam-hosssain <siyam.cybersoul@gmail.com>
2026-07-02 13:22:50 +0600

    java part 1 completed

---
+++ Java-Part-01/004_Clean_Coding/001_Creating_Methods/CreatingMethods/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(greetUser("Siyam","Hossain"));
+
+    }
+    public static String greetUser(String firstName, String lastName){
+        return ("Hello "+firstName+" "+lastName);
+    }
+}
