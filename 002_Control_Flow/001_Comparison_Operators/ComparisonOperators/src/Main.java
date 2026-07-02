321865d1046bc516cb0165ac5af80e583da60b64
siyam-hosssain <siyam.cybersoul@gmail.com>
2026-07-02 08:38:14 +0600

    Comparison operators

---
+++ 002_Control_Flow/001_Comparison_Operators/ComparisonOperators/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 1;
+
+        System.out.println("x == y: "+(x == y));
+        System.out.println("x != y: "+(x != y));
+        System.out.println("x > y: "+(x > y));
+        System.out.println("x >= y: "+(x >= y));
+    }
+}
