f2e5570f7c1c00e416e7c467f3a71fce21745932
siyam-hosssain <siyam.cybersoul@gmail.com>
2026-07-02 13:01:48 +0600

    Debug Java Application

---
+++ 005_Debuggin_and_Deploying/001_Debug_Java_Application/DebugJavaApplication/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start");
+        printNumbers(4);
+        System.out.println("Finish");
+    }
+    public static void printNumbers(int limit){
+        for (int i = 0; i<limit; i++){
+            System.out.println(i);
+        }
+    }
+}
