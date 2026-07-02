d2af514bd84fe039d8be7033e2273a68428c858d
siyam-hosssain <siyam.cybersoul@gmail.com>
2026-07-02 16:51:22 +0600

    Encapsulation

---
+++ Java-Part-02/001_Classes/005_Encapsulation/Encapsulation/src/com/sh/Employee.java
@@ -0,0 +1,10 @@
+package com.sh;
+
+public class Employee {
+    public int baseSalary;
+    public int hourlyRate;
+
+    public int calculateWage(int extraHours){
+        return baseSalary + (hourlyRate * extraHours);
+    }
+}
