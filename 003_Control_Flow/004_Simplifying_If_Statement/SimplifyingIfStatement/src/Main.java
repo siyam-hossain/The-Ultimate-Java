cd68ac9cc626d70f467607184d9f436353e604f8
siyam-hosssain <siyam.cybersoul@gmail.com>
2026-07-02 09:08:08 +0600

    Simplifying If statement

---
+++ 003_Control_Flow/004_Simplifying_If_Statement/SimplifyingIfStatement/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int income = 20_000;
+        boolean hasHighIncome = (income > 100_000);
+
+        System.out.println("has high income: "+ hasHighIncome);
+    }
+}
