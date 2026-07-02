83b95134115d58def2112f471982656d09458e51
siyam-hosssain <siyam.cybersoul@gmail.com>
2026-07-02 08:55:41 +0600

    If statement

---
+++ 003_Control_Flow/003_If_Statement/IfStatement/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int temp = 30;
+        if (temp>30){
+            System.out.println("It's a hot day");
+            System.out.println("Drink water");
+        } else if (temp > 20) {
+            System.out.println("Beautiful day");
+        } else{
+            System.out.println("It is not a hot day");
+        }
+    }
+}
