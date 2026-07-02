70b7936a18e3cccdaa562339b6b2999724b3d657
siyam-hosssain <siyam.cybersoul@gmail.com>
2026-07-02 13:20:18 +0600

    Packaging Java Application

---
+++ 005_Debuggin_and_Deploying/002_Packaging_Java_Application/PackagingJavaApplication/src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        /*
+            1. select file
+            2. project structure
+            3. artifacts
+            4. then select JAR
+            5. then modules with dependencies
+            6. it should open a new window
+            7. select Main class
+            8. then select ok
+            9. Now go to the Build menu
+            10. select build artifacts
+            11. select build
+            12. to run that go to the .jar file path
+            13. run this command: java -jar jar_file_name.jar
+         */
+        System.out.println("Hello world");
+    }
+}
