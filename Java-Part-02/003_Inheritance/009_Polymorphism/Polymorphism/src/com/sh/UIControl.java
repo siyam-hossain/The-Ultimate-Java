8f7eac1d1f7351eeeeca2f7278e2ca2618be6106
siyam-hosssain <siyam.cybersoul@gmail.com>
2026-07-04 09:00:18 +0600

    Ploymorphism

---
+++ Java-Part-02/003_Inheritance/009_Polymorphism/Polymorphism/src/com/sh/UIControl.java
@@ -0,0 +1,24 @@
+package com.sh;
+
+public class UIControl {
+    private boolean isEnabled = true;
+
+//    public UIControl(boolean isEnabled) {
+//        this.isEnabled = isEnabled;
+//        //System.out.println("UI Control");
+//    }
+
+    public void render(){
+
+    }
+
+    public void enable(){
+        isEnabled = true;
+    }
+    public void disable(){
+        isEnabled = false;
+    }
+    public boolean isEnabled(){
+        return isEnabled;
+    }
+}
